e557a388542eabb578d147cd341cefbb71de6431
Jorgemoraa <jorgemoratallavita@gmail.com>
2025-02-14 17:14:08 +0100

    completadas todas las funciones pero con error al ejecutar en el main

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,6 +1,7 @@
 from typing import List, Tuple, Dict, Generator
 from collections import Counter
 import torch
+import random
 
 try:
     from src.utils import tokenize
@@ -23,7 +24,7 @@ def load_and_preprocess_data(infile: str) -> List[str]:
 
     # Preprocess and tokenize the text
     # TODO
-    tokens: List[str] = None
+    tokens: List[str] = tokenize(text)
 
     return tokens
 
@@ -39,13 +40,13 @@ def create_lookup_tables(words: List[str]) -> Tuple[Dict[str, int], Dict[int, st
         and the second maps integers to words (int_to_vocab).
     """
     # TODO
-    word_counts: Counter = None
+    word_counts: Counter = Counter(words)
     # Sorting the words from most to least frequent in text occurrence.
-    sorted_vocab: List[int] = None
+    sorted_vocab: List[int] = sorted(word_counts, key=lambda key: word_counts[key], reverse=True)
     
     # Create int_to_vocab and vocab_to_int dictionaries.
-    int_to_vocab: Dict[int, str] = None
-    vocab_to_int: Dict[str, int] = None
+    int_to_vocab: Dict[int, str] = {indx: word for indx, word in enumerate(sorted_vocab)}
+    vocab_to_int: Dict[str, int] = {word: indx for indx, word in enumerate(sorted_vocab)}
 
     return vocab_to_int, int_to_vocab
 
@@ -71,10 +72,12 @@ def subsample_words(words: List[str], vocab_to_int: Dict[str, int], threshold: f
     """
     # TODO
     # Convert words to integers
-    int_words: List[int] = None
-    
-    freqs: Dict[str, float] = None
-    train_words: List[str] = None
+    # int_words: List[int] = None
+    word_count = Counter(words)
+    total_words = len(words)
+    freqs: Dict[str, float] = {word: count / total_words for word, count in word_count.items()}
+    probs = {word: (1-(threshold/freq) ** 0.5) for word, freq in freqs.items()}
+    train_words: List[int] = [vocab_to_int[word] for word in freqs if freqs[word] > probs[word]]
 
     return train_words, freqs
 
@@ -91,11 +94,14 @@ def get_target(words: List[str], idx: int, window_size: int = 5) -> List[str]:
         List[str]: A list of words selected randomly within the window around the target word.
     """
     # TODO
-    target_words: List[str] = None
+    rand_window = random.randint(1, window_size)
+    start = max(0, idx - rand_window)
+    end = min(len(words), idx + rand_window + 1)
+    target_words: List[str] = [words[i] for i in range(start, end) if i != idx]
 
     return target_words
 
-def get_batches(words: List[int], batch_size: int, window_size: int = 5) -> Generator[Tuple[List[int], List[int]]]:
+def get_batches(words: List[int], batch_size: int, window_size: int = 5):
     """Generate batches of word pairs for training.
 
     This function creates a generator that yields tuples of (inputs, targets),
@@ -116,7 +122,15 @@ def get_batches(words: List[int], batch_size: int, window_size: int = 5) -> Gene
 
     # TODO
     for idx in range(0, len(words), batch_size):
-        inputs, targets: Tuple[List[int], List[int]] = None, None
+        inputs, targets = [], []
+        batch = words[idx: idx + window_size]
+        for i, word in enumerate(batch):
+            target = get_target(batch, i, window_size)
+            # inputs += [word] * len(target)
+            # targets += target
+            for t in target:
+                inputs.append(word)
+                targets.append(t)
         yield inputs, targets
 
 def cosine_similarity(embedding: torch.nn.Embedding, valid_size: int = 16, valid_window: int = 100, device: str = 'cpu'):
@@ -141,7 +155,9 @@ def cosine_similarity(embedding: torch.nn.Embedding, valid_size: int = 16, valid
     """
 
     # TODO
-    valid_examples: torch.Tensor = None
-    similarities: torch.Tensor = None
+    valid_examples: torch.Tensor = torch.randint(low=0, high=valid_window, size=(valid_size,), device=device)
+    valid_embeddings = embedding(valid_examples)
+    weights = embedding.weight
+    similarities: torch.Tensor = (valid_embeddings @ weights.T) / (torch.norm(valid_embeddings) * torch.norm(weights))
 
     return valid_examples, similarities
